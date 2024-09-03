7fe55fea67e33125cefefd895002ec166fec92c2
Yuji181181 <s1f102402552@iniad.org>
2024-09-04 00:52:17 +0900

    値取得、初期化

--- ToDo-js/index1.mjs
+++ ToDo-js/index1.mjs
@@ -1,2 +1,22 @@
 import "./styles1.css";
 
+const onClickAdd = () => {
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
