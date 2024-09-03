7b05ec9bf0bc142928667b964088244e23cabd33
Yuji181181 <s1f102402552@iniad.org>
2024-09-04 06:01:00 +0900

    関数以降

--- ToDo-js/index1.js
+++ ToDo-js/index1.js
@@ -1,7 +1,9 @@
 const onClickAdd = () => {
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value = "";
+}
 
+const createIncompleteTodo = (todo) => {
     const li = document.createElement("li");
 
     const div = document.createElement("div");
@@ -9,7 +11,7 @@ const onClickAdd = () => {
 
     const p =document.createElement("p");
     p.className ="todo-item";
-    p.innerText = inputText;
+    p.innerText = todo;
 
     const completeButton = document.createElement("button");
     completeButton.innerText = "完了";
@@ -20,6 +22,9 @@ const onClickAdd = () => {
 
         const backButton = document.createElement("button");
         backButton.innerText = "戻す";
+        backButton.addEventListener("click",() => {
+
+        });
         moveTarget.firstElementChild.appendChild(backButton);
 
         document.getElementById("complete-list").appendChild(moveTarget);
@@ -38,7 +43,9 @@ const onClickAdd = () => {
     li.appendChild(div);
 
     document.getElementById("incomplete-list").appendChild(li);
-}
+};
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("add-button").addEventListener("click", onClickAdd);
