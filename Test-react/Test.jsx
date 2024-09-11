b6f0d377b6f2bbf8de960e6f8e2dba7361c16506
Yuji181181 <s1f102402552@iniad.org>
2024-09-11 16:41:10 +0900

    テンプレ

--- Test-react/Test.jsx
+++ Test-react/Test.jsx
@@ -1,1 +1,9 @@
+//テンプレート
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
+createRoot(document.getElementById('root')).render(
+// 内容(メイン)
+)
