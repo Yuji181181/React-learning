d5843d09b3edc1fc3a38bea8e0768fdc39799b44
Yuji181181 <s1f102402552@iniad.org>
2024-09-12 01:28:02 +0900

    props

--- Test-react/Test.jsx
+++ Test-react/Test.jsx
@@ -24,3 +24,18 @@ export const App =() => {
 };
 //main.jsxでAppを打つと自動importになる
 
+
+/**
+ * props(コンポーネントの引数のこと)
+ */              
+//components内のファイル
+export const Student = ({name,age,gender}) => { // → アロー関数
+    return (            // ↑ props(分割代入を使用)
+        <>
+            <h3>{name}</h3>
+            <p>{age}</p>
+            <p>{gender}</p>
+        </>
+    );
+};
+
