11911ca3b7e95d6dbb03b847aedd234a677d3164
Yuji181181 <s1f102402552@iniad.org>
2024-09-11 17:21:07 +0900

    ;修正

--- Test-react/Test.jsx
+++ Test-react/Test.jsx
@@ -8,19 +8,19 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
 // 内容(メイン)
-)
+);
 
 
 /**
  * コンポーネントの作り方(App.jsx)
  */
-const App =() => {
+export const App =() => {  
     return(
         <>                     //divタグと選択
             <h1>こんいちは</h1>
             <p>元気？</p>
         </>                    //divタグと選択
-    )
-}
-
+    );
+};
+//main.jsxでAppを打つと自動importになる
 
